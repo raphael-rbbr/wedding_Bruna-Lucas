b9c4236961f3c2897bc3296ab26ee9d5e14afd06
Raphael Breitschaft <raphael.breitschaft@gmail.com>
2022-06-29 16:16:45 -0300

    text on links

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   updateNavbar() {
-    if (window.scrollY >= window.scrollTop() >= 5)  {
+    if (window.scrollY)  {
       this.element.classList.add("nav.navbar-opaco")
     } else {
       this.element.classList.remove("nav.navbar-opaco")
